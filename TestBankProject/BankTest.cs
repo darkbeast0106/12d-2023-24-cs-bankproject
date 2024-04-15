3ab4ad57647d0f4e947d12a2d079796788da00f1
Budaházi Bence <budahazi.bence@petrik.hu>
2024-04-15 13:15:02 +0200

    Első teszteset hozzáadása

---
+++ TestBankProject/BankTest.cs
@@ -0,0 +1,15 @@
+﻿using BankProject;
+
+namespace TestBankProject
+{
+    internal class BankTest
+    {
+        [Test]
+        public void UjSzamla_LetrehozottSzamlaEgyenlege0()
+        {
+            Bank bank = new Bank();
+            bank.UjSzamla("Gipsz Jakab", "1234");
+            Assert.That(bank.Egyenleg("1234"), Is.Zero);
+        }
+    }
+}
---
+++ TestBankProject/UnitTest1.cs
@@ -0,0 +1,28 @@
+//namespace TestBankProject
+//{
+//    public class Tests
+//    {
+//        [SetUp]
+//        public void Setup()
+//        {
+//        }
+
+//        [Test]
+//        public void Test1()
+//        {
+//            Assert.Pass();
+//        }
+
+//        [Test]
+//        public void Test2()
+//        {
+//            Assert.Fail();
+//        }
+          
+//        // Nem jelenik meg a test-exploreren, mert nincs tesztnek megjelölve
+//        public void Test3()
+//        {
+//            Assert.Pass();
+//        }
+//    }
+//}
